ce9732117a703426e66b0df5fd4e21ddda8b18d0
prabhjot-2 <prabh1146@gmail.com>
2024-08-30 23:45:02 +0530

    Company routes added and working fine

--- backend/index.js
+++ backend/index.js
@@ -4,6 +4,8 @@ import cors from "cors"
 import dotenv from "dotenv"
 import connectDb from "./utils/db.js";
 import userRoute from "./routes/user.route.js"
+import companyRoute from "./routes/company.route.js"
+
 
 dotenv.config({});
 
@@ -24,6 +26,9 @@ const PORT= process.env.PORT|| 3001;
 //api's
 app.use("/api/v1/user",userRoute);
 // "htttp://localhost:7999/api/v1/user/register"
+
+app.use("/api/v1/company",companyRoute);
+
 
 app.listen(PORT,()=>{
     connectDb();

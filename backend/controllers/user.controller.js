f7f7f609e9c74b54fadcfebaf1cd9e74da66424d
prabhjot-2 <prabh1146@gmail.com>
2024-08-30 22:45:00 +0530

    All user api working

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -120,16 +120,19 @@ export const updateProfile= async(req,res)=>{
     try {
         const{fullname, email, phoneNumber, bio, skills}=req.body;
         const file=req.file;
-        if(!fullname || !email|| !phoneNumber||!bio||!skills){
-            return res.status(400).json({
-                message:"something is ,issing",
-                success:false
-            });
-        };
+        // if(!fullname || !email|| !phoneNumber||!bio||!skills){
+        //     return res.status(400).json({
+        //         message:"something is ,issing",
+        //         success:false
+        //     });
+        // };
 
         //cloudinary setup
 
-        const skillsArray= skills.split(",");
+        let skillsArray;
+        if(skills){
+            skillsArray= skills.split(",");
+        }
         const userId=req.id;//middleware authentication
         let user = await User.findById(userId);
 
@@ -141,12 +144,12 @@ export const updateProfile= async(req,res)=>{
         }
 
         //updating data
-        user.fullname=fullname,
-        user.email=email,
-        user.phoneNumber=phoneNumber,
-        user.profile.bio=bio,
-        user.profile.skills=skillsArray
-
+        if(fullname) user.fullname=fullname
+        if(email) user.email=email
+        if(phoneNumber) user.phoneNumber=phoneNumber
+        if(bio) user.profile.bio=bio
+        if(skills) user.profile.skills=skillsArray
+        
         //resume implementation  after cloudinary
 
         await user.save();
--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -20,7 +20,7 @@ const userSchema=new mongoose.Schema({
     },
     role:{
         type:String,
-        enum:['student', 'recruiter'],
+        enum:['student', 'recuriter'],
         required:true
     },
     profile:{

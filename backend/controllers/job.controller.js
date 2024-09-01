752c96945fd6d242d5da5c4465547bc10a828374
prabhjot-2 <prabh1146@gmail.com>
2024-09-01 23:58:49 +0530

    job controller and job route  completed Testing is pending

---
+++ backend/controllers/job.controller.js
@@ -0,0 +1,121 @@
+import { Job } from "../models/job.model.js";
+// admin post karega job
+
+export const postJob = async (req, res) => {
+  try {
+    const {
+      title,
+      description,
+      requirements,
+      salary,
+      location,
+      jobType,
+      experience,
+      position,
+      companyId,
+    } = req.body;
+    const userId = req.id;
+
+    if (
+      !title ||
+      !description ||
+      !requirements ||
+      !salary ||
+      !location ||
+      !jobType ||
+      !experience ||
+      !position ||
+      !companyId
+    ) {
+      return res.status(400).json({
+        message: "Somethin is missing.",
+        success: false,
+      });
+    }
+    const job = await Job.create({
+      title,
+      description,
+      requirements: requirements.split(","),
+      salary: Number(salary),
+      location,
+      jobType,
+      experienceLevel: experience,
+      position,
+      company: companyId,
+      created_by: userId,
+    });
+    return res.status(201).json({
+      message: "New job created successfully.",
+      job,
+      success: true,
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+//student k liye
+export const getAllJobs = async (req, res) => {
+  try {
+    const keyword = req.query.keyword || ""; //query.keyword  e hai ki jado url hovega te keyowrd hove ode ch jheri value hovegi jida frontend developer oo sirf show karega
+
+    const query = {
+      $or: [
+        { title: { $reges: keyword, $options: "i" } },
+        { description: { $regex: keyword, $options: "i" } }, // i da mtlb ki case insensitive jo keyword hovega
+      ],
+    };
+
+    const jobs = await Job.find(query); // use populate which is very imporatnt
+    if (!jobs) {
+      return res.status(404).json({
+        message: "Jobs not found",
+        success: true,
+      });
+    }
+
+    return res.status(200).json({
+      jobs,
+      success: true,
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+//studen k liye
+export const getJobById = async (req, res) => {
+  try {
+    const jobId = req.params.id;
+    const job = await Job.findById(jobId);
+    if (!job) {
+      return res.status(404).json({
+        message: "JObs not found",
+        success: true,
+      });
+    }
+
+    return res.status(200).json({ job, success: true });
+  } catch (error) {
+    console.log(error);
+  }
+};
+// admin kitne job create kra hai abhi tak
+
+export const getAdminJobs = async (req, res) => {
+  try {
+    const adminId = req.id;
+    const jobs = await Job.find({ created_by: adminId });
+    if (!jobs) {
+      return res.status(404).json({
+        message: "Jobs not found ",
+        success: false,
+      });
+    }
+
+    return res.status(200).json({
+      jobs,
+      success: true,
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
--- backend/index.js
+++ backend/index.js
@@ -5,6 +5,8 @@ import dotenv from "dotenv"
 import connectDb from "./utils/db.js";
 import userRoute from "./routes/user.route.js"
 import companyRoute from "./routes/company.route.js"
+import jobRoute from "./routes/job.route.js"
+
 
 
 dotenv.config({});
@@ -28,6 +30,8 @@ app.use("/api/v1/user",userRoute);
 // "htttp://localhost:7999/api/v1/user/register"
 
 app.use("/api/v1/company",companyRoute);
+app.use("/api/v1/job",jobRoute);
+
 
 
 app.listen(PORT,()=>{

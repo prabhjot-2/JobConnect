5d6d784a1d1422bbfea65f1f518eb305d4ccc782
prabhjot-2 <prabh1146@gmail.com>
2024-09-04 15:29:49 +0530

    Navbar

---
+++ frontend/src/components/shared/Navbar.jsx
@@ -0,0 +1,69 @@
+import React from "react";
+import { Popover, PopoverContent, PopoverTrigger } from "../ui/popover";
+import { Avatar, AvatarImage } from "../ui/avatar";
+import { Button } from "../ui/button";
+import { LogOut, User2 } from "lucide-react";
+
+const Navbar = () => {
+  return (
+    <div className="bg-grey">
+      <div className=" flex items-center justify-between mx-auto max-w-7xl h-16">
+        <div>
+          <h1 className="text-2xl font-bold">
+            Job<span className="text-[#F83002]">portal </span>
+          </h1>
+        </div>
+        <div className="flex items-center gap-12">
+          <ul className="flex font-medium items-center gap-5">
+            <li>Home</li>
+            <li>Jobs</li>
+            <li>Browse</li>
+          </ul>
+
+          <Popover>
+            <PopoverTrigger asChild>
+              <Avatar className="cursor-pointer">
+                <AvatarImage
+                  src="https://github.com/shadcn.png"
+                  alt="@shadcn"
+                />
+              </Avatar>
+            </PopoverTrigger>
+            <PopoverContent className="w-80">
+              <div className="">
+                <div className="flex gap-2 space-y-2">
+                  <Avatar className="cursor-pointer">
+                    <AvatarImage
+                      src="https://github.com/shadcn.png"
+                      alt="@shadcn"
+                    />
+                  </Avatar>
+                  <div>
+                    <h4 className="font-medium"> Prabhjot Singh</h4>
+                    <p className="text-sm text-muted-foreground">
+                      jdvjdbvh jsbjksbdvjsbfjs
+                    </p>
+                  </div>
+                  
+                </div>
+                <div className="flex flex-col my-2 text-gray-600">
+                    
+                    <div className="flex w-fit items-center gap-2 cursor-pointer">
+                    <User2/>
+                    <Button variant="link"> View Profile</Button>
+                    </div>
+                    <div className="flex w-fit items-center gap-2 cursor-pointer">
+                      <LogOut/>
+                      <Button variant="link"> Logout</Button>
+                    </div>
+                  </div>
+              </div>
+            </PopoverContent>
+          </Popover>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;

57e9e5fbe8ec9cb63a72320dacb37a98394dd979
prabhjot-2 <prabh1146@gmail.com>
2024-09-07 16:32:24 +0530

    signup page complete and working when some user is signup

--- frontend/src/components/auth/Signup.jsx
+++ frontend/src/components/auth/Signup.jsx
@@ -4,7 +4,10 @@ import { Label } from "../ui/label";
 import { Input } from "../ui/input";
 import { RadioGroup } from "../ui/radio-group";
 import { Button } from "../ui/button";
-import { Link } from "react-router-dom";
+import { Form, Link, useNavigate } from "react-router-dom";
+import axios, { formToJSON } from "axios";
+import { USER_API_END_POINT } from '@/utils/constant'
+import { toast } from "sonner";
 
 const Signup = () => {
 
@@ -17,6 +20,8 @@ const Signup = () => {
         file:""
     });
 
+    const navigate=useNavigate();
+
     const changeEventHandler=(e)=>{
         setInput({...input, [e.target.name]:e.target.value});
     }
@@ -26,7 +31,38 @@ const Signup = () => {
 
     const submitHandler= async(e)=>{
         e.preventDefault();
-        console.log(input)
+        // console.log(input)
+        const formData=new FormData();
+        formData.append("fullname", input.fullname)
+        formData.append("email", input.email)
+        formData.append("phoneNumber", input.phoneNumber)
+        formData.append("password", input.password)
+        formData.append("role", input.role)
+
+
+        if(input.file){
+          formData.append("file", input.file);
+        }
+
+
+        try {
+          const res= await axios.post(`${USER_API_END_POINT}/register`,formData,{
+            headers:{
+              "Content-Type":"multipart/form-data"
+            },
+            withCredentials:true,
+          });
+
+          if(res.data.success){
+            navigate("/login")
+            toast.success(res.data.message)
+          }
+        } catch (error) {
+          console.log(error);
+          toast.error(error.response.data.message)
+
+          
+        }
     }
   return (
     <div>

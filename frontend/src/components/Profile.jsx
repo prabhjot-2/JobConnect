7c7a38e90ee6595b9ef76c1173686b7d880298ee
prabhjot-2 <prabh1146@gmail.com>
2024-09-15 15:35:01 +0530

    now user can update there data

--- frontend/src/components/Profile.jsx
+++ frontend/src/components/Profile.jsx
@@ -1,75 +1,71 @@
-import React from "react";
-import Navbar from "./shared/Navbar";
+import React, { useState } from 'react'
+import Navbar from './shared/Navbar'
+import { Avatar, AvatarImage } from './ui/avatar'
+import { Button } from './ui/button'
+import { Contact, Mail, Pen } from 'lucide-react'
+import { Badge } from './ui/badge'
+import { Label } from './ui/label'
+import AppliedJobTable from './AppliedJobTable'
+import UpdateProfileDialog from './UpdateProfileDialog'
+import { useSelector } from 'react-redux'
 
-import { Avatar, AvatarImage } from "./ui/avatar";
-import { Button } from "./ui/button";
-import { Contact, Mail, Pen, Underline } from "lucide-react";
-import { Badge } from "./ui/badge";
-import { Label } from "./ui/label";
-import AppliedJobTable from "./AppliedJobTable";
-
-const skills=["Html","Css","Javascript","Reactjs"]
+// const skills = ["Html", "Css", "Javascript", "Reactjs"]
+const isResume = true;
 
 const Profile = () => {
 
-    const isResume=true;
-  return (
-    <div>
-      <Navbar />
-      <div className="max-w-4xl mx-auto bg-white border border-gray-200 rounded-2xl my-5 p-8">
-        <div className="flex justify-between">
-          <div className="flex items-center gap-4">
-            <Avatar className="h-24 w-24">
-              <AvatarImage
-                src="https://th.bing.com/th/id/R.ceaf8384322af790486cff176a0a2f24?rik=miqL9LHAiG2FBQ&riu=http%3a%2f%2fwww.freepngimg.com%2fdownload%2fmicrosoft%2f28350-5-microsoft-logo-photos.png&ehk=gLNeUUMG1tz9FSTh4v1gEOdCeSICRm2gC8CoPEk9eU0%3d&risl=&pid=ImgRaw&r=0"
-                alt="Profile Photo"
-              />
-            </Avatar>
-            <div>
-              <h1 className="font-medium text-xl">Full Name</h1>
-              <p>in here bio is tehre pkeseae enter the bio in here</p>
-            </div>
-          </div>
-          <Button className="text-right" variant="outline">
-            <Pen />
-          </Button>
-        </div>
-        <div className="my-5">
-          <div className="flex items-center gap-3 my-2">
-            <Mail />
-            <span>prabh@gmail.com</span>
-          </div>
-          <div className="flex items-center gap-3 my-2">
-            <Contact />
-            <span>9877463625</span>
-          </div>
-        </div>
-        <div className="my-5">
-            <h1>Skills</h1>
-            <div className="flex items-center gap-1"> 
-            {
-                skills.length!=0 ?skills.map((item,index)=><Badge key={index}>{item}</Badge>): NA
-            }
-            </div>
-            
-        </div>
-        <div className="grid w-full max-w-sm items-center gap-1.5">
-            <Label>Resume</Label>
-            {
-                isResume? <a target="blank" href="https://google.com" className="text-blue-500 w-full hover:underline cursor-pointer"> Resume Link</a>: <span>NA</span>
-            }
-        </div>
-        
-      </div>
-      <div className="max-w-4xl mx-auto bg-white rounded-2xl"> 
-            <h1 className="font-bold text-lg my-5">
-                Applied Jobs
-                {/* Application Table */}
-            </h1>
-            <AppliedJobTable/>
-        </div>
-    </div>
-  );
-};
+    const [open, setOpen] = useState(false);
+    const {user} = useSelector(store=>store.auth);
 
-export default Profile;
+    return (
+        <div>
+            <Navbar />
+            <div className='max-w-4xl mx-auto bg-white border border-gray-200 rounded-2xl my-5 p-8'>
+                <div className='flex justify-between'>
+                    <div className='flex items-center gap-4'>
+                        <Avatar className="h-24 w-24">
+                            <AvatarImage src="https://www.shutterstock.com/image-vector/circle-line-simple-design-logo-600nw-2174926871.jpg" alt="profile" />
+                        </Avatar>
+                        <div>
+                            <h1 className='font-medium text-xl'>{user?.fullname}</h1>
+                            <p>{user?.profile?.bio}</p>
+                        </div>
+                    </div>
+                    <Button onClick={() => setOpen(true)} className="text-right" variant="outline"><Pen /></Button>
+                </div>
+                <div className='my-5'>
+                    <div className='flex items-center gap-3 my-2'>
+                        <Mail />
+                        <span>{user?.email}</span>
+                    </div>
+                    <div className='flex items-center gap-3 my-2'>
+                        <Contact />
+                        <span>{user?.phoneNumber}</span>
+                    </div>
+                </div>
+                <div className='my-5'>
+                    <h1>Skills</h1>
+                    <div className='flex items-center gap-1'>
+                        {
+                            user?.profile?.skills.length !== 0 ? user?.profile?.skills.map((item, index) => <Badge key={index}>{item}</Badge>) : <span>NA</span>
+                        }
+                    </div>
+                </div>
+                <div className='grid w-full max-w-sm items-center gap-1.5'>
+                    <Label className="text-md font-bold">Resume</Label>
+                    {
+                        isResume ? <a target='blank' href={user?.profile?.resume} className='text-blue-500 w-full hover:underline cursor-pointer'>{user?.profile?.resumeOriginalName}</a> : <span>NA</span>
+                    }
+                </div>
+            </div>
+            <div className='max-w-4xl mx-auto bg-white rounded-2xl'>
+                <h1 className='font-bold text-lg my-5'>Applied Jobs</h1>
+                {/* Applied Job Table   */}
+                <AppliedJobTable />
+            </div>
+            <UpdateProfileDialog open={open} setOpen={setOpen}/>
+        </div>
+    )
+}
+
+export default Profile

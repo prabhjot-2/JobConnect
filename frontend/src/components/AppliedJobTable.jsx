f5c4e1d06730c335d51d005fa9ccfe9fead08e3b
prabhjot-2 <prabh1146@gmail.com>
2024-09-23 23:41:04 +0530

    Applied Jobs dynamic status works

--- frontend/src/components/AppliedJobTable.jsx
+++ frontend/src/components/AppliedJobTable.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
 import { Table, TableBody, TableCaption, TableCell, TableHead, TableHeader, TableRow } from './ui/table'
 import { Badge } from './ui/badge'
-import { Item } from '@radix-ui/react-radio-group'
+import { useSelector } from 'react-redux'
 
 const AppliedJobTable = () => {
+    const {allAppliedJobs} = useSelector(store=>store.job);
     return (
         <div>
             <Table>
@@ -18,12 +19,12 @@ const AppliedJobTable = () => {
                 </TableHeader>
                 <TableBody>
                     {
-                        [1,2].map((item,index) => (
-                            <TableRow key={index}>
-                                <TableCell>13-09-2024</TableCell>
-                                <TableCell>Fronted Developer</TableCell>
-                                <TableCell>google</TableCell>
-                                <TableCell className="text-right"><Badge>Selected</Badge></TableCell>
+                        allAppliedJobs.length <= 0 ? <span>You haven't applied any job yet.</span> : allAppliedJobs.map((appliedJob) => (
+                            <TableRow key={appliedJob._id}>
+                                <TableCell>{appliedJob?.createdAt?.split("T")[0]}</TableCell>
+                                <TableCell>{appliedJob.job?.title}</TableCell>
+                                <TableCell>{appliedJob.job?.company?.name}</TableCell>
+                                <TableCell className="text-right"><Badge className={`${appliedJob?.status === "rejected" ? 'bg-red-400' : appliedJob.status === 'pending' ? 'bg-gray-400' : 'bg-green-400'}`}>{appliedJob.status.toUpperCase()}</Badge></TableCell>
                             </TableRow>
                         ))
                     }

fe6ae959457dba7674acba8d8347fc0bbd283cb7
prabhjot-2 <prabh1146@gmail.com>
2024-09-24 23:38:28 +0530

    created protected route inn which now only student can access to only student pages and admin to there pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,7 @@
-
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import Navbar from './components/shared/Navbar'
-
-import Signup from './components/auth/Signup'
 import Login from './components/auth/Login'
+import Signup from './components/auth/Signup'
 import Home from './components/Home'
 import Jobs from './components/Jobs'
 import Browse from './components/Browse'
@@ -11,77 +9,75 @@ import Profile from './components/Profile'
 import JobDescription from './components/JobDescription'
 import Companies from './components/admin/Companies'
 import CompanyCreate from './components/admin/CompanyCreate'
-import CompanySetup from './components/admin/CompnaySetup'
-import AdminJobs from './components/admin/AdminJobs'
+import CompanySetup from './components/admin/CompanySetup'
+import AdminJobs from "./components/admin/AdminJobs";
 import PostJob from './components/admin/PostJob'
 import Applicants from './components/admin/Applicants'
+import ProtectedRoute from './components/admin/ProtectedRoute'
 
 
-
-
-
-const appRouter= createBrowserRouter([
+const appRouter = createBrowserRouter([
   {
-    path:"/",
-    element:<Home/>
+    path: '/',
+    element: <Home />
   },
   {
-    path:"/login",
-    element:<Login/>
+    path: '/login',
+    element: <Login />
   },
   {
-    path:"/signup",
-    element:<Signup/>
+    path: '/signup',
+    element: <Signup />
   },
   {
-    path:"/jobs",
-    element:<Jobs/>
+    path: "/jobs",
+    element: <Jobs />
   },
   {
-    path:"/description/:id",
-    element:<JobDescription/>
+    path: "/description/:id",
+    element: <JobDescription />
   },
   {
-    path:"/browse",
-    element:<Browse/>
+    path: "/browse",
+    element: <Browse />
   },
   {
-    path:"/profile",
-    element:<Profile/>
+    path: "/profile",
+    element: <Profile />
   },
+  // admin ke liye yha se start hoga
   {
     path:"/admin/companies",
-    element: <Companies/>
+    element: <ProtectedRoute><Companies/></ProtectedRoute>
   },
   {
     path:"/admin/companies/create",
-    element: <CompanyCreate/>
+    element: <ProtectedRoute><CompanyCreate/></ProtectedRoute> 
   },
   {
     path:"/admin/companies/:id",
-    element: <CompanySetup/>
+    element:<ProtectedRoute><CompanySetup/></ProtectedRoute> 
   },
   {
     path:"/admin/jobs",
-    element: <AdminJobs/>
+    element:<ProtectedRoute><AdminJobs/></ProtectedRoute> 
   },
   {
     path:"/admin/jobs/create",
-    element: <PostJob/>
+    element:<ProtectedRoute><PostJob/></ProtectedRoute> 
   },
   {
     path:"/admin/jobs/:id/applicants",
-    element: <Applicants/>
+    element:<ProtectedRoute><Applicants/></ProtectedRoute> 
   },
 
 ])
-
 function App() {
 
   return (
-    <>
-      <RouterProvider router={appRouter}/>
-    </>
+    <div>
+      <RouterProvider router={appRouter} />
+    </div>
   )
 }
 

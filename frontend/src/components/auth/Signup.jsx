bcb49cc2589bb153d2c626aa14773a6618de0121
prabhjot-2 <prabh1146@gmail.com>
2024-09-25 23:04:16 +0530

    admin page works for recuriter and other pages works for students respectively

--- frontend/src/components/auth/Signup.jsx
+++ frontend/src/components/auth/Signup.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Navbar from "../shared/Navbar";
 import { Label } from "../ui/label";
 import { Input } from "../ui/input";
@@ -23,7 +23,7 @@ const Signup = () => {
   });
 
   const navigate = useNavigate();
-  const { loading } = useSelector((store) => store.auth);
+  const { loading , user} = useSelector((store) => store.auth);
   const dispatch = useDispatch();
   const changeEventHandler = (e) => {
     setInput({ ...input, [e.target.name]: e.target.value });
@@ -66,6 +66,11 @@ const Signup = () => {
       dispatch(setLoading(false));
     }
   };
+  useEffect(()=>{
+    if(user){
+      navigate("/");
+    }
+  },[])
   return (
     <div>
       <Navbar />
